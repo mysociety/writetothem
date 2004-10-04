a85e82bc575c0993d9073722c3af93a4bfd9ca02
francis <francis>
2004-10-04 15:02:30 +0000

    Version 1 of FaxYourRepresentative

---
+++ web/index.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+<title>Fax Your Representative</title>
+</head>
+
+
+<body>
+
+<h1>Fax Your Representative</h1>
+
+<p>To fax your representative please ring directory enquiries (118 118).
+Ask for your representatives fax number.  Then write your fax on a piece
+of paper, and send it to them using your fax machine.
+
+</body>
+
+</html>
+
