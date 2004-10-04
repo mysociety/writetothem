abe521b34ed7ac40b7e7f0d9d4c3aacbfcbbd4b3
francis <francis>
2004-10-04 16:28:17 +0000

    Chris's mockup pages

---
+++ web/whotofax.php
@@ -0,0 +1,225 @@
+
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<TITLE>FaxYourRepresentative.com - Fax or Email Your Democratic Representatives For Free</TITLE>
+<LINK REL="STYLESHEET" TYPE="text/css" HREF="fax_style.css">
+</HEAD>
+<BODY BGCOLOR="#FFFFFF">
+<DIV ALIGN="center">
+<TABLE CELLSPACING="0" CELLPADDING="0" BORDER="0" WIDTH="530">
+<TR>
+<TD VALIGN="top" ALIGN="center">
+<IMG SRC="mosaic.gif" WIDTH="529" HEIGHT="12" BORDER="0" ALT="mosaic_strip"><BR><BR>
+<!--
+<TABLE CELLPADDING="4" CELLSPACING="2" BORDER="0" WIDTH="100%">
+<TR>
+<TD BGCOLOR="#cccc99" ALIGN="center"><A HREF="/index.php3">Home</A></TD>
+<TD BGCOLOR="#cccc99" ALIGN="center"><A HREF="/youandyourmp.php3">You &amp; Your MP</A></TD>
+<TD BGCOLOR="#cccc99" ALIGN="center"><A HREF="/q_a.php3">Q&amp;A</A></TD>
+<TD BGCOLOR="#cccc99" ALIGN="center"><A HREF="/about_us.php3">About Us</A></TD>
+<TD BGCOLOR="#cccc99" ALIGN="center"><A HREF="/contact.php3">Contact Us</A></TD>
+</TR>
+</TABLE>
+-->
+<H3>FaxYourRepresentative.com - Fax or Email Your Democratic Representatives For Free</H3>
+
+
+<form method="get" action="dofax.html">
+
+<P align="left" style="align: left;">You have several democratic representatives at different levels of government.
+Please choose the most appropriate one to contact, and then click any of the buttons
+marked "Next".</p>
+
+<p align="left"><b>If you aren't sure who to contact, please read <a href="whoadvice1.html">this advice</a>.</b></p>
+
+<table>
+
+<tr><td colspan="2" align="center" height="4" bgcolor="#cecece"></td></tr><tr>
+    <td width="50%">
+    <h4>Your City Councillors</h4>
+    <p>Your City Councillors represent you on the <a href="http://www.cambridge.gov.uk/">Cambridge City Council</a>.
+    Your City Council is responsible for local services and policy, including 
+    planning, council housing, building regulation, rubbish collection, and local
+    roads. Some
+    responsibilities, such as recreation facilities, are shared with the County
+    Council. (<a href="citycouncils.html">Find out more</a>.)</p>
+    </td><td valign="bottom">
+    <p>In your ward, <b>West Chesterton</b>, you are represented by three councillors.
+    Please choose one councillor to contact.</p>
+    <table style="float: left;">
+        <tr>
+            <td valign="top"><input type="radio" name="who" value="cc1"></td>
+            <td><b>Diane Armstrong</b><br>Liberal Democrat</td>
+            <td valign="top"><input type="radio" name="who" value="cc2"></td>
+            <td><b>Max Boyce</b><br>Liberal Democrat</td>
+        </tr>
+
+        <tr>
+            <td valign="top"><input type="radio" name="who" value="cc3"></td>
+            <td><b>Ian Nimmo-Smith</b><br>Liberal Democrat</td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+    <input style="float: right" type="submit" value="Next &gt;&gt;">
+    </td>
+</tr>
+
+<tr><td colspan="2" align="center" height="4" bgcolor="#cecece"></td></tr><tr>
+    <td>
+    <h4>Your County Councillor</h4>
+    <p>Your County Councillor represents you on the <a href="http://www.cambridgeshire.gov.uk">Cambridgeshire
+    County Council</a>. Your County Council is responsible for local services,
+    including education, social services, transport and libraries. (<a href="countycouncils.html">Find out more</a>).</p>
+    </td><td valign="bottom">
+    <p>In your ward, <b>West Chesterton</b>, your County Councillor is:</p>
+    <table style="float: left;">
+    <tr>
+    <td valign="top"><input type="radio" name="who" value="Cc"></td>
+    <td><b>Maurice Leek</b><br>Liberal Democrat</td>
+    </table>
+    <input style="float: right" type="submit" value="Next &gt;&gt;">
+    </td>
+</tr>
+
+<tr><td colspan="2" align="center" height="4" bgcolor="#cecece"></td></tr><tr>
+    <td>
+    <h4>Your MP</h4>
+    <p>Your MP represents you in the <a href="http://www.parliament.uk/about_commons/about_commons.cfm">House of Commons</a> in Westminster.
+    The House of Commons is responsible for making laws in the UK and
+    for overall scrutiny of all aspects of government.</p>
+    </td><td valign="bottom">
+    <p>In your constituency, <b>Cambridge</b>, your MP is:</p>
+    <table style="float: left">
+    <tr>
+    <td valign="top"><input type="radio" name="who" value="mp"></td>
+    <td><b>Anne Campbell</b><br>Labour Party</td>
+    </table>
+    <input style="float: right" type="submit" value="Next &gt;&gt;">
+    </td>
+</tr>
+
+<tr><td colspan="2" align="center" height="4" bgcolor="#cecece"></td></tr><tr>
+    <td valign="top">
+    <h4>Your MEPs (Member of European Parliament)</h4>
+    <p>MEPs represent you in the <a href="http://www.europarl.eu.int/opengov/default_noscript_en.htm">European Parliament</a>. They
+    scrutinise European laws (called "directives") and the budget of the
+    European Union, and provides oversight of the other decision-making bodies
+    of the Union, including the Council of Ministers and the Commission.</p>
+    </td><td valign="bottom">
+    <p>In your European Constituency, <b>Eastern Region</b>, MEPs are elected
+    through a party list system. You are represented by seven MEPs.
+    Please choose one MEP to contact:</p>
+    <table style="float: left">
+        <tr>
+            <td valign="top"><input type="radio" name="who" value="mep1"></td>
+            <td><b>Geoffrey van Orden</b><br>Conservative</td>
+            <td valign="top"><input type="radio" name="who" value="mep2"></td>
+            <td><b>Jeffrey Titford</b><br>UK Independence Party</td>
+        </tr>
+
+        <tr>
+            <td valign="top"><input type="radio" name="who" value="mep3"></td>
+            <td><b>Richard Howitt</b><br>Labour Party</td>
+            <td valign="top"><input type="radio" name="who" value="mep4"></td>
+            <td><b>Robert Sturdy</b><br>Conservative Party</td>
+        </tr>
+
+        <tr>
+            <td valign="top"><input type="radio" name="who" value="mep5"></td>
+            <td><b>Andrew Duff</b><br>Liberal Democrat</td>
+            <td valign="top"><input type="radio" name="who" value="mep6"></td>
+            <td><b>Christopher Beazley</b><br>Conservative Party</td>
+        </tr>
+
+        <tr>
+            <td valign="top"><input type="radio" name="who" value="mep7"></td>
+            <td><b>Tom Wise</b><br>UK Independence Party</td>
+            <td></td><td></td>
+        </tr>
+    </table>
+    <input style="float: right" type="submit" value="Next &gt;&gt;">
+    
+    </td>
+</tr>
+
+</table>
+
+
+<!--
+
+	<P><BR>
+<TABLE CELLPADDING="2" CELLSPACING="2" BORDER="0">
+	<TR>
+		<TD CLASS="white" BGCOLOR="#cccc99" ALIGN="center">1</TD>
+		<TD COLSPAN="4"><B>Type in your full UK postcode, then click "Go"</B></TD>
+	</TR>
+	<TR>
+		<TD COLSPAN="5"><IMG SRC="/images/tiny.gif" ALT="" WIDTH="1" HEIGHT="5" BORDER="0"></TD>
+	</TR>
+	<TR>
+		<TD><IMG SRC="/images/tiny.gif" ALT="" WIDTH="1" HEIGHT="1" BORDER="0"></TD>
+		<TD CLASS="white" BGCOLOR="#999966" ALIGN="center">2</TD>
+		<TD COLSPAN="3"><B>Write the letter you want to be faxed to your Councillor</B></TD>
+	</TR>
+	<TR>
+		<TD COLSPAN="5"><IMG SRC="/images/tiny.gif" ALT="" WIDTH="1" HEIGHT="5" BORDER="0"></TD>
+	</TR>
+	<TR>
+		<TD COLSPAN="2"><IMG SRC="/images/tiny.gif" ALT="" WIDTH="1" HEIGHT="1" BORDER="0"></TD>
+		<TD CLASS="white" BGCOLOR="#99cc99" ALIGN="center">3</TD>
+		<TD COLSPAN="2"><B>Click the link in the email we'll send you</B></TD>
+	</TR>
+	<TR>
+		<TD COLSPAN="5"><IMG SRC="/images/tiny.gif" ALT="" WIDTH="1" HEIGHT="5" BORDER="0"></TD>
+	</TR>
+	<TR>
+		<TD COLSPAN="3"><IMG SRC="/images/tiny.gif" ALT="" WIDTH="1" HEIGHT="1" BORDER="0"></TD>
+		<TD CLASS="white" BGCOLOR="#000066" ALIGN="center">4</TD>
+		<TD><B>That's it! You have faxed your Councillor</B></TD>
+	</TR>
+	<TR>
+		<TD><IMG SRC="/images/tiny.gif" ALT="" WIDTH="14" HEIGHT="1" BORDER="0"></TD>
+		<TD><IMG SRC="/images/tiny.gif" ALT="" WIDTH="14" HEIGHT="1" BORDER="0"></TD>
+		<TD><IMG SRC="/images/tiny.gif" ALT="" WIDTH="14" HEIGHT="1" BORDER="0"></TD>
+		<TD><IMG SRC="/images/tiny.gif" ALT="" WIDTH="14" HEIGHT="1" BORDER="0"></TD>
+		<TD><IMG SRC="/images/tiny.gif" ALT="" WIDTH="200" HEIGHT="1" BORDER="0"></TD>
+	</TR>
+	</TABLE><P>
+-->
+
+
+<H4>
+12345 faxes sent to date.
+<P>
+       <P ALIGN="center"><FONT SIZE="2" COLOR="#FF0000"><B>
+Sending identical copied and pasted "form" letters will do your cause more harm than good...
+<a href="/q_a.php3#formletters">Here's why.</A></B></FONT></P>
+
+
+</H4>
+<!--
+	<A HREF="feedback.php3">Does It Really Work?</a>
+	&nbsp;&nbsp;||&nbsp;&nbsp;		
+	<A HREF="link_to_us.php3">How To Link To Us</a> 
+	&nbsp;&nbsp;||&nbsp;&nbsp;
+	<A HREF="/guidelines.php3">Guidelines for Campaigners</a> 
+	<P>
+-->
+<IMG SRC="mosaic.gif" WIDTH="529" HEIGHT="12" BORDER="0" ALT="mosaic_strip"><BR><P>
+</TD>
+	</TR>
+	<TR>
+		<TD><IMG SRC="/images/tiny.gif" ALT="" WIDTH="530" HEIGHT="1" BORDER="0"></TD>
+	</TR>
+</TABLE><P>
+</DIV>
+
+
+</script><noscript><img src=http://x3.extreme-dm.com/z/?tag=hollypk&p=http%3A%2F%2Fwww.faxyourmp.com%2Findex.php3&j=n height=1 width=1></noscript>
+</BODY>
+</HTML>
+
