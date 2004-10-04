d331f8da8f5f0b8f1a965c898c1e3a4180fa6dab
chris <chris>
2004-10-04 17:07:31 +0000

    Uses VA_ constants.

--- lib/mapit.php
+++ lib/mapit.php
@@ -6,7 +6,7 @@
  * Copyright (c) 2004 Chris Lightfoot. All rights reserved.
  * Email: chris@ex-parrot.com; WWW: http://www.ex-parrot.com/~chris/
  *
- * $Id: mapit.php,v 1.3 2004-10-04 17:04:19 chris Exp $
+ * $Id: mapit.php,v 1.4 2004-10-04 17:07:31 chris Exp $
  * 
  */
 
@@ -26,15 +26,18 @@ function get_voting_areas($postcode) {
     if (!preg_match('/[A-Z]{1,2}[0-9]{1,4}[A-Z]{1,2}/', $postcode)) {
         return MAPIT_BAD_POSTCODE;
     } else if ($postcode == 'CB41EP') {
-        return array(CTY => array(1, 'Cambridgeshire County Council'),
-                     CED => array(2, 'West Chesterton ED'),
+        return array(VA_CTY => array(1, 'Cambridgeshire County Council'),
+                     VA_CED => array(2, 'West Chesterton ED'),
 
-                     DIS => array(3, 'Cambridge District Council'),
-                     DIW => array(4, 'West Chesterton Ward',
+                     VA_DIS => array(3, 'Cambridge District Council'),
+                     VA_DIW => array(4, 'West Chesterton Ward'),
 
-                     WMC => array(5, 'Cambridge'),
-
-                     EUR => array(6, 'Eastern Euro Region');
+                     VA_WMP => $va_name(VA_WMP),
+                     VA_WMC => array(5, 'Cambridge'),
+            
+                     VA_EUP => $va_name(VA_EUP),
+                     VA_EUR => array(6, 'Eastern Euro Region')
+                );
     } else {
         return MAPIT_NOT_FOUND;
     }
