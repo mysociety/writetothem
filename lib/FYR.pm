c99e8e082ea66a5ba3174504617d5f08d4986581
chris <chris>
2004-10-05 14:45:22 +0000

    ""

--- lib/FYR.pm
+++ lib/FYR.pm
@@ -6,7 +6,7 @@
 # Copyright (c) 2004 UK Citizens Online Democracy. All rights reserved.
 # Email: chris@mysociety.org; WWW: http://www.mysociety.org/
 #
-# $Id: FYR.pm,v 1.2 2004-10-05 14:47:41 chris Exp $
+# $Id: FYR.pm,v 1.1 2004-10-05 14:45:22 chris Exp $
 #
 
 package FYR::Error;
@@ -41,28 +41,13 @@ package FYR::DB;
 use DBI;
 use FYR::Config;
 
-=item new_dbh
-
-Return a new handle on the database.
-
-=cut
-sub new_dbh () {
-    return DBI->connect('dbi:Pg:%s' . FYR::Config::get_value('dbname', 'fyr'),
-                        FYR::Config::get_value('dbuser', 'fyr'),
-                        FYR::Config::get_value('dbpass', ''),
-                        { RaiseError => 1, AutoCommit => 0 });
-}
-
-
-=item dbh
-
-Return a shared handle on the database.
-
-=cut
 sub dbh () {
     our $dbh;
-    $dbh ||= another_dbh();
-    return $dbh;
+    if (!$dbh) {
+        $dbh = DBI->connect('dbi:Pg:%s' . FYR::Config::get_value('dbname', 'fyr'),
+                            FYR::Config::get_value('dbuser', 'fyr'),
+                            FYR::Config::get_value('dbpass', ''), {
+    }
 }
 
 1;
