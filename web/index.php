9263128f76bd595545fc7cb921c525afdf2a4a53
francis <francis>
2004-10-15 14:19:34 +0000

    Learning about QuickForm

--- web/index.php
+++ web/index.php
@@ -5,12 +5,23 @@
  * Copyright (c) 2004 UK Citizens Online Democracy. All rights reserved.
  * Email: francis@mysociety.org. WWW: http://www.mysociety.org
  *
- * $Id: index.php,v 1.6 2004-10-05 20:35:51 francis Exp $
+ * $Id: index.php,v 1.7 2004-10-15 14:19:34 francis Exp $
  * 
  */
 
+require_once "../phplib/forms.php";
+
+$form = new HTML_QuickForm('postcodeForm', 'get', 'who.php');
+$form->addElement('text', 'pc', "Type Your UK Postcode:", array('size' => 10, 'maxlength' => 255));
+$form->addElement('submit', 'go', 'Go');
+$form->applyFilter('pc', 'trim');
+$form->addRule('pc', 'Please enter your postcode', 'required', null, null);
+$form->setDefaults(array('pc' => "CB4 1XP"));
+
+$fyr_form_renderer = forms_custom_renderer();
+$form->accept($fyr_form_renderer);
+
 $fyr_title = "Fax or Email Your Democratic Representatives For Free";
-$fyr_initial_postcode = "CB4 1XP";
 include "templates/index.html";
 
 ?>
