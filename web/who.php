7ba66b35b32d4bd776f051a57a48a49f5c2357f0
francis <francis>
2004-10-19 17:20:10 +0000

    Correctly display plural

--- web/who.php
+++ web/who.php
@@ -5,7 +5,7 @@
  * Copyright (c) 2004 UK Citizens Online Democracy. All rights reserved.
  * Email: francis@mysociety.org. WWW: http://www.mysociety.org
  *
- * $Id: who.php,v 1.14 2004-10-19 16:46:08 francis Exp $
+ * $Id: who.php,v 1.15 2004-10-19 17:20:10 francis Exp $
  * 
  */
 
@@ -89,10 +89,11 @@ foreach ($voting_areas as $va_type => $va_specificid) {
     $rep_count = count($representatives);
     
     // Create HTML
-    $left_column = "<h4>Your ${va_info['rep_name']}</h4><p>";
     if ($rep_count > 1) {
+        $left_column = "<h4>Your ${va_info['rep_name_plural']}</h4><p>";
         $left_column .= "Your ${va_info['rep_name_plural']} represent you ${eb_info['attend_prep']} ";
     } else {
+        $left_column = "<h4>Your ${va_info['rep_name']}</h4><p>";
         $left_column .= "Your ${va_info['rep_name']} represents you ${eb_info['attend_prep']} ";
     }
     $left_column .= "${eb_info['name']}.  ${eb_info['description']}.</p>";
