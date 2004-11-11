409a01e640bb56e577ba908eec5639a10af82835
francis <francis>
2004-11-11 12:54:27 +0000

    Server info admin page

--- web/admin.php
+++ web/admin.php
@@ -5,7 +5,7 @@
  * Copyright (c) 2004 UK Citizens Online Democracy. All rights reserved.
  * Email: francis@mysociety.org. WWW: http://www.mysociety.org
  *
- * $Id: admin.php,v 1.1 2004-11-11 12:24:47 francis Exp $
+ * $Id: admin.php,v 1.2 2004-11-11 12:54:27 francis Exp $
  * 
  */
 
@@ -15,6 +15,7 @@ require_once "../../phplib/admin.php";
 $pages = array(
     new ADMIN_PAGE_RATTY,
     new ADMIN_PAGE_PHPINFO,
+    new ADMIN_PAGE_SERVERINFO,
 );
 
 admin_page_display("FaxYourRepresentative", $pages);
