434903b73b28d4719a550f88ccaf87a98334b57b
francis <francis>
2004-11-18 23:43:15 +0000

    Tom's email

--- web-admin/index.php
+++ web-admin/index.php
@@ -5,7 +5,7 @@
  * Copyright (c) 2004 UK Citizens Online Democracy. All rights reserved.
  * Email: francis@mysociety.org. WWW: http://www.mysociety.org
  *
- * $Id: index.php,v 1.2 2004-11-18 21:32:32 francis Exp $
+ * $Id: index.php,v 1.3 2004-11-18 23:43:15 francis Exp $
  * 
  */
 
@@ -16,9 +16,10 @@ require_once "../../phplib/admin.php";
 $pages = array(
     new ADMIN_PAGE_FYR_QUEUE,
     new ADMIN_PAGE_RATTY,
-    new ADMIN_PAGE_PHPINFO,
+    null, // space separator on menu
     new ADMIN_PAGE_SERVERINFO,
     new ADMIN_PAGE_CONFIGINFO,
+    new ADMIN_PAGE_PHPINFO,
 );
 
 admin_page_display("FaxYourRepresentative", $pages);
