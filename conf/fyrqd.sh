e64fc721b70659bf54a46d6bd9577f3efdc23e28
fyr <fyr>
2004-11-18 20:36:22 +0000

    Updated

--- conf/fyrqd.sh
+++ conf/fyrqd.sh
@@ -3,7 +3,7 @@
 # $FreeBSD: ports/net/rsync/files/rsyncd.sh,v 1.2 2004/08/12 10:45:27 eik Exp $
 #
 
-# FreeBSD file to put in /usr/local/etc/rc.d for starting fyrqd
+# FreeBSD script for FYR queue daemon.  Put it in /usr/local/etc/rc.d
 
 # PROVIDE: fyrqd
 # REQUIRE: LOGIN
@@ -24,4 +24,5 @@ load_rc_config "$name"
 
 : ${fyrqd_user="fyr"}
 : ${fyrqd_chdir="/data/vhost/notfinished.faxyourrepresentative.com/fyr/bin"}
+: ${command_interpreter="/usr/bin/perl"}
 run_rc_command "$1"
