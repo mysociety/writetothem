5398da1ed347ce6162d166ce5cda6c332cae68f7
francis <francis>
2004-11-19 12:25:44 +0000

    Beginnings of dadem and mapit admin interfaces

--- web-admin/index.php
+++ web-admin/index.php
@@ -5,7 +5,7 @@
  * Copyright (c) 2004 UK Citizens Online Democracy. All rights reserved.
  * Email: francis@mysociety.org. WWW: http://www.mysociety.org
  *
- * $Id: index.php,v 1.3 2004-11-18 23:43:15 francis Exp $
+ * $Id: index.php,v 1.4 2004-11-19 12:25:44 francis Exp $
  * 
  */
 
@@ -16,6 +16,8 @@ require_once "../../phplib/admin.php";
 $pages = array(
     new ADMIN_PAGE_FYR_QUEUE,
     new ADMIN_PAGE_RATTY,
+    new ADMIN_PAGE_MAPIT,
+    new ADMIN_PAGE_DADEM,
     null, // space separator on menu
     new ADMIN_PAGE_SERVERINFO,
     new ADMIN_PAGE_CONFIGINFO,
