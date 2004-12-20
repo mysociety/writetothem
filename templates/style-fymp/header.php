ccd3fe5ea1c876c048f610ec7ccd22556dbc51f4
francis <francis>
2004-12-20 22:46:07 +0000

    Search for representative. Reverse lookup example postcode.

--- templates/style-fymp/header.php
+++ templates/style-fymp/header.php
@@ -1,3 +1,4 @@
+<?php header("Content-Type:", "text/html; charset=utf-8"); ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html lang="en">
 <head>
