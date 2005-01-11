02a9a529a3258da946db4dc2924540e5dc811800
chris <chris>
2005-01-11 16:14:51 +0000

    Adapt about.php to also show "problem" pages displayed for abusive messages; also various small fixes/tweaks to that page; also change rewrite rule to work for /problem-... as well as /about-....

--- web/about.php
+++ web/about.php
@@ -1,24 +1,36 @@
 <?
 /*
  * about.php:
- * Wrapper for displaying templated about pages.
+ * Wrapper for displaying templated about/problem pages.
  * 
  * Copyright (c) 2004 UK Citizens Online Democracy. All rights reserved.
  * Email: francis@mysociety.org. WWW: http://www.mysociety.org
  *
- * $Id: about.php,v 1.3 2005-01-11 15:23:55 chris Exp $
+ * $Id: about.php,v 1.4 2005-01-11 16:14:51 chris Exp $
  * 
  */
 
 require_once "../phplib/fyr.php";
 
-# See special about rewrite rule which maps
-# "about-qa" to "about.php?page=qa"
+/* 
+ * This page is called through a rewrite mechanism which maps
+ *   /$template_name
+ * to
+ *   /about.php?page=$template_name
+ * for about-... and problem-... templates.
+ */
 
 $page = get_http_var("page");
-$page = preg_replace("/([a-z]+)/", "$1", $page); // safety first
-if (!$page) $page = 'index';
-template_draw("about-" . $page, array());
+if (!isset($page) || !preg_match('/^(?:about|problem)-[a-z]+$/', $page))
+    $page = 'index';
+
+$values = array();
+
+/* Don't want problem pages to be indexed, really. */
+if (preg_match('/^problem-/', $page))
+    $values['robots'] = 'noindex, nofollow';
+
+template_draw($page, $values);
 
 ?>
 
