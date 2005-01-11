9d418cba88f081a7f058e79cdc9b5aef31912ffc
chris <chris>
2005-01-11 15:23:55 +0000

    "If the code and the comments disagree, then both are probably wrong."

--- web/about.php
+++ web/about.php
@@ -1,18 +1,19 @@
 <?
 /*
- * Main page of FaxYourRepresentative, where you enter your postcode
+ * about.php:
+ * Wrapper for displaying templated about pages.
  * 
  * Copyright (c) 2004 UK Citizens Online Democracy. All rights reserved.
  * Email: francis@mysociety.org. WWW: http://www.mysociety.org
  *
- * $Id: about.php,v 1.2 2005-01-11 00:29:06 matthew Exp $
+ * $Id: about.php,v 1.3 2005-01-11 15:23:55 chris Exp $
  * 
  */
 
 require_once "../phplib/fyr.php";
 
 # See special about rewrite rule which maps
-# "about/qa" to "about.php?page=qa"
+# "about-qa" to "about.php?page=qa"
 
 $page = get_http_var("page");
 $page = preg_replace("/([a-z]+)/", "$1", $page); // safety first
