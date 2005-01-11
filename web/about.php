2efc51627cc24b89bfefca0c84a6d3d99d778c53
matthew <matthew>
2005-01-11 23:30:09 +0000

    No verb to message; bug fix for About index page

--- web/about.php
+++ web/about.php
@@ -6,7 +6,7 @@
  * Copyright (c) 2004 UK Citizens Online Democracy. All rights reserved.
  * Email: francis@mysociety.org. WWW: http://www.mysociety.org
  *
- * $Id: about.php,v 1.8 2005-01-11 16:59:58 chris Exp $
+ * $Id: about.php,v 1.9 2005-01-11 23:30:10 matthew Exp $
  * 
  */
 
@@ -24,7 +24,7 @@ require_once "../phplib/fyr.php";
 
 $page = get_http_var("page");
 if (!isset($page) || (!preg_match('/^(?:about|problem)-[a-z]+$/', $page) && $page != 'write-checkemail' && $page != 'confirm-accept'))
-    $page = 'index';
+    $page = 'about-index';
     
 $values = array();
 
