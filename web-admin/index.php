de0b95f05a165ea664a0e9fe885e29f515c80984
chris <chris>
2005-01-13 02:19:23 +0000

    More (and corrected) blurb for rate limiting admin.

--- web-admin/index.php
+++ web-admin/index.php
@@ -1,11 +1,12 @@
 <?
 /*
- * Admin pages for FaxYourRepresentative.
+ * index.php:
+ * Admin index for FaxYourRepresentative.
  * 
  * Copyright (c) 2004 UK Citizens Online Democracy. All rights reserved.
  * Email: francis@mysociety.org. WWW: http://www.mysociety.org
  *
- * $Id: index.php,v 1.11 2005-01-12 18:03:12 francis Exp $
+ * $Id: index.php,v 1.12 2005-01-13 02:19:23 chris Exp $
  * 
  */
 
@@ -15,11 +16,34 @@ require_once "../../phplib/admin.php";
 
 $pages = array(
     new ADMIN_PAGE_FYR_QUEUE(),
-    new ADMIN_PAGE_RATTY('fyr-web', "WTT Website", "These rules limit
-        access to the WriteToThem website."),
-    new ADMIN_PAGE_RATTY('fyr-abuse', "Message Abuse", "These rules
-        apply to messages when they are first submitted onto the message
-        queue by the user."),
+    new ADMIN_PAGE_RATTY(
+            'fyr-web',
+            "WTT Website",
+            "These rules limit access to the WriteToThem website.",
+            <<<EOF
+HTML fragment to be displayed to user on error page when rule fires. If left
+blank, the message "Sorry, we are experiencing technical difficulties" is
+displayed.
+EOF
+        ),
+    new ADMIN_PAGE_RATTY(
+            'fyr-abuse',
+            "Message Abuse",
+            "These rules apply to messages when they are first submitted onto the message queue by the user.",
+            <<<EOF
+What to do with a message for which this rule fires. This should be either:
+<dl>
+<dt>freeze</dt>
+<dd>to freeze the message for inspection by an administrator (preventing
+delivery); or</dd>
+<dt><em>TEMPLATE</em></dt>
+<dd>to drop (abandon) the message and display the named <em>TEMPLATE</em>
+(for instance 'problem-similar' will display
+<a href="http://www.writetothem.com/problem-similar">the notice about
+cut-and-paste messages</a>).</dd>
+</dl>
+EOF
+            ),
     new ADMIN_PAGE_REPS,
     null, // space separator on menu
     new ADMIN_PAGE_SERVERINFO,
