1f1bb433b3764487d32e75d4393d78e22974d938
matthew <matthew>
2005-02-08 10:49:54 +0000

    Text for linking to corrections interface

--- web/who.php
+++ web/who.php
@@ -6,7 +6,7 @@
  * Copyright (c) 2004 UK Citizens Online Democracy. All rights reserved.
  * Email: francis@mysociety.org. WWW: http://www.mysociety.org
  *
- * $Id: who.php,v 1.51 2005-01-24 21:52:09 matthew Exp $
+ * $Id: who.php,v 1.52 2005-02-08 10:49:54 matthew Exp $
  * 
  */
 
@@ -156,6 +156,9 @@ foreach ($va_display_order as $va_type) {
                 . " not paid for the work they do.</em></p>";
         if ($va_type == 'WMC') {
             $text .= '<p id="twfy"><a href="http://www.theyworkforyou.com/mp/?c=' . urlencode(str_replace(' and ',' &amp; ',$va_info['name'])) . '">Find out more about ' . $representatives_info[$representatives[0]]['name'] . ' at TheyWorkForYou.com</a></p>';
+        }
+        if (in_array($va_type, array('DIW','CED','LGW','LBW','UTE','UTW','MTW'))) {
+            $text .= '<p><a href="corrections?id='.$va_specificid.'">Do you know of mistakes in the above list?</a></p>';
         }
     }
 
