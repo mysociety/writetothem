317b327bcdf41c5a04edbf8d52a45d7a7d51bd40
francis <francis>
2005-02-09 11:53:20 +0000

    Don't moan if a token is confirmed twice, just print confirmation message again.

--- web/confirm.php
+++ web/confirm.php
@@ -7,7 +7,7 @@
  * Copyright (c) 2004 UK Citizens Online Democracy. All rights reserved.
  * Email: francis@mysociety.org. WWW: http://www.mysociety.org
  *
- * $Id: confirm.php,v 1.10 2004-12-15 15:35:04 francis Exp $
+ * $Id: confirm.php,v 1.11 2005-02-09 11:53:20 francis Exp $
  * 
  */
 
@@ -26,9 +26,12 @@ if (!$token) {
 
 $result = msg_confirm_email($token);
 if (rabx_is_error($result)) {
-    if ($result->code == FYR_QUEUE_MESSAGE_ALREADY_CONFIRMED) 
-        template_show_error("Thanks, but you've already confirmed your message.");
-    template_show_error($result->text);
+    if ($result->code == FYR_QUEUE_MESSAGE_ALREADY_CONFIRMED) {
+        // Don't moan when message was already confirmed
+        $result = true;
+    } else {
+        template_show_error($result->text);
+    }
 }
 if (!$result) {
     template_draw("confirm-trouble");
