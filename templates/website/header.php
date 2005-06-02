f780a5328549db221ef8bf4ab4cef71b021d1be4
matthew <matthew>
2005-06-02 21:15:20 +0000

    Syndication. Will improve templating RSN.

--- templates/website/header.php
+++ templates/website/header.php
@@ -1,31 +1,32 @@
-<?php header("Content-Type: text/html; charset=utf-8"); ?>
+<?php
+
+$syn = str_replace('.writetothem.com', '', $_SERVER['SERVER_NAME']);
+
+header("Content-Type: text/html; charset=utf-8"); ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html lang="en">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>WriteToThem.com Beta Test - <? print $values['title']; ?></title>
-<?php
-if (array_key_exists('css', $_REQUEST) && !preg_match('#"#',$_REQUEST['css'])) { ?>
-<style type="text/css">
-@import "<?=$_REQUEST['css'] ?>";
-</style>
-<?php
-} else { ?>
 <link href="/wtt.css" rel="stylesheet" type="text/css" media="all">
-<?php
-}
+<?php if ($syn == 'cheltenham') { ?>
+<style type="text/css">@import "cheltenham.css";</style>
+<?php }
 if (array_key_exists('robots', $values)) { ?>
 <meta name="robots" content="<?=$values['robots'] ?>">
 <?php } ?>
 </head>
 <body><a name="top" id="top"></a>
+<?php if ($syn == 'cheltenham') 
+        print '<a title="Back to Cheltenham Council website" href="http://www.cheltenham.gov.uk/"><img id="chelt" alt="Return to www.cheltenham.gov.uk" src="http://www.cheltenham.gov.uk/libraries/images/logo.gif"></a>';
+?>
 <h1 id="heading"><? if ($_SERVER['REQUEST_URI']!='/') print '<a href="/">'; ?>
 WriteToThem.com<?
 if ($_SERVER['REQUEST_URI']!='/') print '</a>';
 if (OPTION_FYR_REFLECT_EMAILS) {
-    print ' <span id="betatest">Staging Site</span>';
+    print ' <span id="betatest">Staging&nbsp;Site</span>';
 } else {
-    print ' <span id="betatest">Beta Test</span>';
+    print ' <span id="betatest">Beta&nbsp;Test</span>';
 }
 ?>
 </h1>
