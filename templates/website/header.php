88283185a9d2879a777ce70229f7d709106b9f97
francis <francis>
2005-06-03 09:07:37 +0000

    Override syndication with GET parameter for testing

--- templates/website/header.php
+++ templates/website/header.php
@@ -1,6 +1,11 @@
 <?php
 
+# syndication type, read from domain name
 $syn = str_replace('.writetothem.com', '', $_SERVER['HTTP_HOST']);
+# or override for debugging
+if (array_key_exists('syn', $_GET)) {
+    $syn = $_GET['syn'];
+}
 
 header("Content-Type: text/html; charset=utf-8"); ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
