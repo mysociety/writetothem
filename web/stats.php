a6f833ee99ef3bf8090e736927138d3c6f56b72a
francis <francis>
2006-02-15 17:26:26 +0000

    Change sort order, and some other bits

--- web/stats.php
+++ web/stats.php
@@ -6,7 +6,7 @@
  * Copyright (c) 2006 UK Citizens Online Democracy. All rights reserved.
  * Email: matthew@mysociety.org. WWW: http://www.mysociety.org
  *
- * $Id: stats.php,v 1.7 2006-02-15 17:05:52 francis Exp $
+ * $Id: stats.php,v 1.8 2006-02-15 17:26:26 francis Exp $
  * 
  */
 require_once '../phplib/fyr.php';
@@ -103,26 +103,32 @@ function mp_response_table($year, $questionnaire_report) {
     }
 
     # Sort data
+    function by_name($a, $b) {
+        return strcmp($a['name'], $b['name']);
+    }
+    function by_area($a, $b) {
+        return strcmp($a['area'], $b['area']);
+    }
+    function by_sent($a, $b) {
+        if ($a['sent']<$b['sent']) return 1;
+        elseif ($a['sent']>$b['sent']) return -1;
+        return 0;
+    }
     $sort = get_http_var('o');
     if ($sort == 'n') {
-        function by_name($a, $b) {
-            return strcmp($a['name'], $b['name']);
-        }
         usort($data['data'], 'by_name');
     } elseif ($sort == 'c') {
-        function by_area($a, $b) {
-            return strcmp($a['area'], $b['area']);
-        }
         usort($data['data'], 'by_area');
     } elseif ($sort == 's') {
-        function by_sent($a, $b) {
-            if ($a['sent']<$b['sent']) return 1;
-            elseif ($a['sent']>$b['sent']) return -1;
-            return 0;
-        }
         usort($data['data'], 'by_sent');
     } else {
         function by_response($a, $b) {
+            if ($a['category'] != 'good' && $b['category'] == 'good')
+                return 1;
+            if ($b['category'] != 'good' && $a['category'] == 'good')
+                return -1;
+            if ($a['category'] != 'good' && $b['category'] != 'good')
+                return by_name($a, $b);
             if ($a['response']<$b['response']) return 1;
             elseif ($a['response']>$b['response']) return -1;
             if ($a['low']<$b['low']) return 1;
@@ -142,11 +148,11 @@ function mp_response_table($year, $questionnaire_report) {
 }
 
 function category_lookup($cat) {
-    if (strstr($cat, 'good')) return '';
+    if ($cat == 'good') return '';
     elseif ($cat == 'shame') return "MP doesn't accept messages via WriteToThem";
     elseif ($cat == 'toofew') return 'Too few messages sent to MP';
     elseif ($cat == 'unknown') return 'We need to manually check this MP';
-    elseif ($cat == 'cheat') return 'This MP attempted improve their response rate by sending themselves messages';
+    elseif ($cat == 'cheat') return 'MP attempted to improve their response rate by sending themselves messages';
     elseif ($cat == 'badcontact') return 'WriteToThem had possibly bad contact details for this MP';
     else template_show_error("Unknown MP categorisation '".htmlspecialchars($cat)."'");
     return $cat;
