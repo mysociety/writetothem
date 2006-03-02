b84c1a4b41416c0b47f4a3fea5fbc31847a716d3
francis <francis>
2006-03-02 17:56:44 +0000

    Fix typo in link to GE data

--- templates/website/footer.php
+++ templates/website/footer.php
@@ -4,7 +4,7 @@ The new FaxYourMP.
 <a href="/about-us">Built by 
 <!--<img src="/images/mysociety_sm.gif" border="0" alt="mySociety" />-->
 mySociety</a>.  
-<a href="about-copyright">Data by GovEval</a>.
+<a href="/about-copyright">Data by GovEval</a>.
 <br>
 <? $links = array(
     '/about-qa'=>'Help',
