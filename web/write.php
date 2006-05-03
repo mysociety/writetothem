7353dd8bd1b74b4d3574a7c8b1fb1aa81284a40e
francis <francis>
2006-05-03 15:56:44 +0000

    Minor formatting change.

--- web/write.php
+++ web/write.php
@@ -6,7 +6,7 @@
  * Copyright (c) 2004 UK Citizens Online Democracy. All rights reserved.
  * Email: francis@mysociety.org. WWW: http://www.mysociety.org
  *
- * $Id: write.php,v 1.101 2006-05-03 15:49:30 chris Exp $
+ * $Id: write.php,v 1.102 2006-05-03 15:56:44 francis Exp $
  *
  */
 
@@ -69,7 +69,7 @@ class RulePostcode extends HTML_QuickForm_Rule {
 
 function compare_email_addrs($F) {
     if ($F['writer_email'] != $F['writer_email2'])
-        return array('writer_email' => "The two email addresses you've entered differ; please check them carefully for mistakes");
+        return array('writer_email' => "The two email addresses you've entered differ;<br>please check them carefully for mistakes");
     return true;
 }
 
