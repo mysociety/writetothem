28eaabd51ede765961aaeafa127f5c3dfac30233
francis <francis>
2006-08-30 23:21:22 +0000

    Alter MaPit.get_voting_area_geometry to return an empty hash where data is missing. Still raises an error if the id is not a valid area id.

--- phplib/queue.php
+++ phplib/queue.php
@@ -8,7 +8,7 @@
  * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
  * WWW: http://www.mysociety.org
  *
- * $Id: queue.php,v 1.57 2006-08-29 10:44:10 francis Exp $
+ * $Id: queue.php,v 1.58 2006-08-30 23:21:22 francis Exp $
  *
  */
 
