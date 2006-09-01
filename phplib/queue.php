2eaf78d9a26db39bbcea209f13ae303c4b177f84
francis <francis>
2006-09-01 11:43:40 +0000

    MaPit.get_voting_areas_geometry can now return boundary info

--- phplib/queue.php
+++ phplib/queue.php
@@ -8,7 +8,7 @@
  * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
  * WWW: http://www.mysociety.org
  *
- * $Id: queue.php,v 1.58 2006-08-30 23:21:22 francis Exp $
+ * $Id: queue.php,v 1.59 2006-09-01 11:43:40 francis Exp $
  *
  */
 
