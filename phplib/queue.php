9dded3ded29dbfbe488e0673651b7e0ebe53d946
francis <francis>
2006-09-01 11:47:14 +0000

    Actually take POLYGON_TYPE option

--- phplib/queue.php
+++ phplib/queue.php
@@ -8,7 +8,7 @@
  * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
  * WWW: http://www.mysociety.org
  *
- * $Id: queue.php,v 1.59 2006-09-01 11:43:40 francis Exp $
+ * $Id: queue.php,v 1.60 2006-09-01 11:47:14 francis Exp $
  *
  */
 
