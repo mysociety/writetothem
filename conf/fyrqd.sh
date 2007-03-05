a6403dbd8dfe12bf280076a84564a3194318da30
francis <francis>
2007-03-05 10:27:07 +0000

    *** empty log message ***

--- conf/fyrqd.sh
+++ conf/fyrqd.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-#
-# $FreeBSD: ports/net/rsync/files/rsyncd.sh,v 1.2 2004/08/12 10:45:27 eik Exp $
-#
 
-# FreeBSD script for FYR queue daemon.  Put it in /usr/local/etc/rc.d
+# NOTE: FOR FAX DAEMON ONLY
+
+# FreeBSD script for FYR queue fax daemon.  
+# Put it in /usr/local/etc/rc.d, and add this to /etc/rc.conf
+#   fyrqd_enable="YES"
+# And then to start it
+#   /usr/local/etc/rc.d/fyrqd.sh start
 
 # PROVIDE: fyrqd
 # REQUIRE: LOGIN
@@ -15,14 +18,14 @@
 name="fyrqd"
 rcvar=`set_rcvar`
 
-command="/data/vhost/notfinished.faxyourrepresentative.com/fyr/bin/fyrqd"
-command_args=""
-pidfile="/data/vhost/notfinished.faxyourrepresentative.com/$name.pid"
+command="/home/fyr/mysociety/fyr/bin/fyrqd"
+command_args="--fax"
+pidfile="/home/fyr/$name.pid"
 
 # read configuration and set defaults
 load_rc_config "$name"
 
 : ${fyrqd_user="fyr"}
-: ${fyrqd_chdir="/data/vhost/notfinished.faxyourrepresentative.com/fyr/bin"}
+: ${fyrqd_chdir="/home/fyr/mysociety/fyr/bin"}
 : ${command_interpreter="/usr/bin/perl"}
 run_rc_command "$1"
