f30196a8b024679aa53682cc49a71248b26f592a
matthew <matthew>
2007-08-18 09:58:01 +0000

    More error avoidances.

--- templates/animalaid/header.php
+++ templates/animalaid/header.php
@@ -1,6 +1,6 @@
 <?php
 
-$values['stylesheet'] = 'animalaid.css';
+$values['stylesheet'] = '/animalaid.css';
 $values['header'] = '<a title="Back to Animal Aid website" href="http://www.animalaid.org.uk/"><img id="cobrand_logo" alt="Return to www.animalaid.org.uk" src="http://www.animalaiduk.com/templates/aa/images/logo.gif"></a>';
 include_once '../templates/common_header.php';
 
--- templates/cheltenham/header.php
+++ templates/cheltenham/header.php
@@ -1,6 +1,6 @@
 <?php
 
-$values['stylesheet'] = 'cheltenham.css';
+$values['stylesheet'] = '/cheltenham.css';
 $values['header'] = '<a title="Back to Cheltenham Council website" href="http://www.cheltenham.gov.uk/"><img id="cobrand_logo" alt="Return to www.cheltenham.gov.uk" src="http://www.cheltenham.gov.uk/libraries/images/logo.gif"></a>';
 include_once '../templates/common_header.php';
 
