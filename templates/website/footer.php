34d3e17692c5f83a703dab37f85f5b0d7bdd00d8
francis <francis>
2008-08-08 15:55:36 +0000

    Log analyse with mySociety piwik installation for writetothem.com

--- templates/website/footer.php
+++ templates/website/footer.php
@@ -32,4 +32,27 @@ global $track;
 if (isset($track) && $track) {
     track_event($track);
 }
+
+if (OPTION_WEB_DOMAIN == 'writetothem.com') {
+?>
+<!-- Piwik -->
+<a href="http://piwik.org" title="Web analytics" onclick="window.open(this.href);return(false);">
+<script type="text/javascript">
+var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.mysociety.org/" : "http://piwik.mysociety.org/");
+document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+<!--
+piwik_action_name = '';
+piwik_idsite = 2;
+piwik_url = pkBaseURL + "piwik.php";
+piwik_log(piwik_action_name, piwik_idsite, piwik_url);
+//-->
+</script><object>
+<noscript><p>Web analytics <img src="http://piwik.mysociety.org/piwik.php" style="border:0" alt="piwik"/></p>
+</noscript></object></a>
+<!-- /Piwik --> 
+<?
+}
+
 echo '</body></html>';
