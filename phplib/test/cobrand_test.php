7c843bcf4444658797f29e2b224dbed723a84455
louise <louise>
2009-09-29 15:04:46 +0000

    First tests

--- phplib/test/cobrand_test.php
+++ phplib/test/cobrand_test.php
@@ -1,12 +1,12 @@
 <?php
 /*
  * SimpleTest tests for the functions in cobrand.php
- * $Id: cobrand_test.php,v 1.2 2009-09-29 14:18:15 louise Exp $
+ * $Id: cobrand_test.php,v 1.3 2009-09-29 15:04:46 louise Exp $
  */
-error_reporting (E_ALL);
+error_reporting (E_ALL ^ E_NOTICE);
 ini_set("display_errors", 1);
 include_once dirname(__FILE__) . '/../../conf/general'; 
-include_once dirname(__FILE__) . '/../fyr.php';
+include_once dirname(__FILE__) . '/../cobrand.php';
 include_once 'simpletest/unit_tester.php';
 include_once 'simpletest/reporter.php';
 
@@ -15,10 +15,14 @@ class CobrandTest extends UnitTestCase{
   function test_cobrand_headers(){
     $headers = cobrand_headers('nosite', 'common_header');
     $this->assertEqual('', $headers, 'Should return an empty string for common_header with no cobrand');
+
+    $headers = cobrand_headers('mysite', 'common_header');
+    $this->assertEqual('My-Header: hello', $headers, 'Should return the value of the cobrand header function if one exists');
   }
  
 } 
 
-$test = new CobrandTest();
-exit ($test->run(new TextReporter()) ? 0 : 1);
+$test = new CobrandTest;
+$reporter = new TextReporter;
+exit ($test->run($reporter) ? 0 : 1);
 ?>
