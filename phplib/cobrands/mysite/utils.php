0c9897b73cc10cf17549625e7df0b0b9dcbca8fa
louise <louise>
2009-09-29 17:06:14 +0000

    Example cobrand php module for testing cobranding functions

---
+++ phplib/cobrands/mysite/utils.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Example cobrand for the functions in cobrand.php
+ * $Id: utils.php,v 1.1 2009-09-29 17:06:14 louise Exp $
+ */
+
+function headers($template) {
+  return 'My-Header: hello';
+}
+
+?>
