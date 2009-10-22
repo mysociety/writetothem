0064048ec700e897ed2d855d56ae498d46dca735
louise <louise>
2009-10-22 09:42:28 +0000

    Fixing indentation

--- phplib/test/cobrand_test.php
+++ phplib/test/cobrand_test.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * SimpleTest tests for the functions in cobrand.php
- * $Id: cobrand_test.php,v 1.8 2009-10-22 09:38:25 louise Exp $
+ * $Id: cobrand_test.php,v 1.9 2009-10-22 09:42:28 louise Exp $
  */
 error_reporting (E_ALL ^ E_NOTICE);
 ini_set("display_errors", 1);
@@ -12,73 +12,74 @@ include_once 'simpletest/reporter.php';
 
 class CobrandTest extends UnitTestCase{
   
-  function test_cobrand_headers(){
-    $headers = cobrand_headers('nosite', 'common_header');
-    $this->assertEqual('', $headers, 'Should return an empty string for common_header with no cobrand');
+    function test_cobrand_headers(){
+        $headers = cobrand_headers('nosite', 'common_header');
+        $this->assertEqual('', $headers, 'Should return an empty string for common_header with no cobrand');
 
-    $headers = cobrand_headers('mysite', 'common_header');
-    $this->assertEqual('My-Header: hello', $headers, 'Should return the value of the cobrand header function if one exists');
-  }
+        $headers = cobrand_headers('mysite', 'common_header');
+        $this->assertEqual('My-Header: hello', $headers, 'Should return the value of the cobrand header function if one exists');
+    }
 
-  function test_display_councillor_correction_link() {
-    $display_link = cobrand_display_councillor_correction_link('');
-    $this->assertEqual(true, $display_link, 'Should return true if no cobrand is set');
-  
-    $display_link = cobrand_display_councillor_correction_link('mysite');
-    $this->assertEqual(false, $display_link, 'Should return the value of the cobrand display_councillor_correction_link function if one exists');
+    function test_display_councillor_correction_link() {
+        $display_link = cobrand_display_councillor_correction_link('');
+        $this->assertEqual(true, $display_link, 'Should return true if no cobrand is set');
 
-    $display_link = cobrand_display_councillor_correction_link('nosite');
-    $this->assertEqual(true, $display_link, 'Should return true if the cobrand does not define a display_councillor_correction_link function');
-  
-  }
- 
-  function test_display_spellchecker() {
+        $display_link = cobrand_display_councillor_correction_link('mysite');
+        $this->assertEqual(false, $display_link, 'Should return the value of the cobrand display_councillor_correction_link function if one exists');
 
-    $display_spell = cobrand_display_spellchecker('');
-    $this->assertEqual(true, $display_spell, 'Should return true if no cobrand is set');
+        $display_link = cobrand_display_councillor_correction_link('nosite');
+        $this->assertEqual(true, $display_link, 'Should return true if the cobrand does not define a display_councillor_correction_link function');
 
-    $display_spell = cobrand_display_spellchecker('mysite');
-    $this->assertEqual(false, $display_spell, 'Should return the value of the cobrand display_spellchecker function if one exists');
+    }
 
-    $display_spell = cobrand_display_spellchecker('nosite');
-    $this->assertEqual(true, $display_spell, 'Should return true if the cobrand does not define a display_spellcheck function');
+    function test_display_spellchecker() {
 
-  }
+        $display_spell = cobrand_display_spellchecker('');
+        $this->assertEqual(true, $display_spell, 'Should return true if no cobrand is set');
 
-  function test_display_survey() {
+        $display_spell = cobrand_display_spellchecker('mysite');
+        $this->assertEqual(false, $display_spell, 'Should return the value of the cobrand display_spellchecker function if one exists');
 
-    $display_survey = cobrand_display_survey('');
-    $this->assertEqual(1, $display_survey, 'Should return true if no cobrand is set');
+        $display_spell = cobrand_display_spellchecker('nosite');
+        $this->assertEqual(true, $display_spell, 'Should return true if the cobrand does not define a display_spellcheck function');
 
-    $display_survey = cobrand_display_survey('mysite');
-    $this->assertEqual(false, $display_survey, 'Should return the value of the cobrand display_survey function if one exists');
+    }
 
-    $display_survey = cobrand_display_survey('nosite');
-    $this->assertEqual(true, $display_survey, 'Should return true if the cobrand does not define a display_survey function');
-  }
+    function test_display_survey() {
 
-  function test_cobrand_page() {
+        $display_survey = cobrand_display_survey('');
+        $this->assertEqual(1, $display_survey, 'Should return true if no cobrand is set');
+
+        $display_survey = cobrand_display_survey('mysite');
+        $this->assertEqual(false, $display_survey, 'Should return the value of the cobrand display_survey function if one exists');
+
+        $display_survey = cobrand_display_survey('nosite');
+        $this->assertEqual(true, $display_survey, 'Should return true if the cobrand does not define a display_survey function');
    
-    $text = cobrand_page('mysite', 'existing_page');
-    $this->assertEqual('page content', $text, 'Should return the content of the cobrand page for pages that exist');
+    }
 
-    $text = cobrand_page('mysite', 'non_existing_page');
-    $this->assertEqual('', $text, 'Should return an empty string for pages that do not exist');
+    function test_cobrand_page() {
 
-    $text = cobrand_page('nosite', 'existing_page');
-    $this->assertEqual('', $text, 'Should return an empty string for cobrands that do not define a page function');
+        $text = cobrand_page('mysite', 'existing_page');
+        $this->assertEqual('page content', $text, 'Should return the content of the cobrand page for pages that exist');
 
-  }
+        $text = cobrand_page('mysite', 'non_existing_page');
+        $this->assertEqual('', $text, 'Should return an empty string for pages that do not exist');
 
-  function test_url() {
+        $text = cobrand_page('nosite', 'existing_page');
+        $this->assertEqual('', $text, 'Should return an empty string for cobrands that do not define a page function');
 
-      $url = cobrand_url('mysite', 'url');
-      $this->assertEqual('rewritten_url', $url, 'Should return the url as rewritten by the cobrand if the cobrand defines a url function');
-   
-      $url = cobrand_url('nosite', 'url');
-      $this->assertEqual('url', $url, 'Should return the url passed if there is no cobrand url function');
+    }
 
-  }
+    function test_url() {
+
+        $url = cobrand_url('mysite', 'url');
+        $this->assertEqual('rewritten_url', $url, 'Should return the url as rewritten by the cobrand if the cobrand defines a url function');
+
+        $url = cobrand_url('nosite', 'url');
+        $this->assertEqual('url', $url, 'Should return the url passed if there is no cobrand url function');
+
+    }
 
 } 
 
