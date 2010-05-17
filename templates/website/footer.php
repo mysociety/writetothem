6e251d0dbbe8338b5bd3195856e05b9b3f09cd2f
Louise Crow <louise.crow@gmail.com>
2010-05-17 17:15:52 +0100

    Updating piwik tracking js

--- templates/website/footer.php
+++ templates/website/footer.php
@@ -39,17 +39,14 @@ if (OPTION_WEB_DOMAIN == 'writetothem.com') {
 <script type="text/javascript">
 var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.mysociety.org/" : "http://piwik.mysociety.org/");
 document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-<!--
-piwik_action_name = '';
-piwik_idsite = 2;
-piwik_url = pkBaseURL + "piwik.php";
-piwik_log(piwik_action_name, piwik_idsite, piwik_url);
-//-->
-</script>
-<noscript><img src="http://piwik.mysociety.org/piwik.php?idsite=2" style="border:0" alt=""></noscript>
-<!-- /Piwik --> 
+</script><script type="text/javascript">
+try {
+var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 2);
+piwikTracker.trackPageView();
+piwikTracker.enableLinkTracking();
+} catch( err ) {}
+</script><noscript><p><img src="http://piwik.mysociety.org/piwik.php?idsite=2" style="border:0" alt=""/></p></noscript>
+<!-- End Piwik Tag -->
 <?
 }
 
