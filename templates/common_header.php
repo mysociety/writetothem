3cfaba5e8898e8471076689242288f7275aaeb83
Matthew Somerville <matthew@mysociety.org>
2012-04-19 16:39:09 +0100

    Add GA to WriteToThem.

--- templates/common_header.php
+++ templates/common_header.php
@@ -34,7 +34,26 @@ $(function(){
 <?php }
       if (array_key_exists('robots', $values)) { ?>
 <meta name="robots" content="<?=$values['robots'] ?>">
+<?php }
+      if (OPTION_WEB_DOMAIN == 'writetothem.com') {
+?>
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '<?=OPTION_GOOGLE_ANALYTICS_TRACKING_CODE?>']);
+  _gaq.push(['_setDomainName', 'writetothem.com']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push (['_gat._anonymizeIp']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 <?php } ?>
+
+
 </head>
 <body<? if (isset($values['body_id'])) print ' id="' . $values['body_id'] . '"'; ?>><a name="top" id="top"></a>
 <?php if (array_key_exists('header', $values)) {
