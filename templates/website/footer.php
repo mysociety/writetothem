013e23b44d2027b8ac281036436456736a65c597
Matthew Somerville <matthew@mysociety.org>
2012-05-14 11:36:43 +0100

    Remove piwik.

--- templates/website/footer.php
+++ templates/website/footer.php
@@ -28,29 +28,4 @@ foreach ($links as $uri => $text) {
 print join(' | ', $footer);
 echo '</div>';
 
-if (OPTION_WEB_DOMAIN == 'writetothem.com') {
-?>
-<!-- Piwik -->
-<script type="text/javascript">
-var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.mysociety.org/" : "http://piwik.mysociety.org/");
-document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
-</script><script type="text/javascript">
-try {
-var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 2);
-<? 
-if ($values['title'] && preg_match('/Now write your message to/', $values['title'])){
-  $values['tracking_title'] = 'Now write your message';
-}else{
-  $values['tracking_title'] = $values['title'];
-}
-?>
-piwikTracker.setDocumentTitle("<?php echo $values['tracking_title'] ?>");
-piwikTracker.trackPageView();
-piwikTracker.enableLinkTracking();
-} catch( err ) {}
-</script><noscript><p><img src="http://piwik.mysociety.org/piwik.php?idsite=2" style="border:0" alt=""/></p></noscript>
-<!-- End Piwik Tag -->
-<?
-}
-
 echo '</body></html>';
