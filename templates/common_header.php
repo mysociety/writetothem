58e370a18ee5c103187d32142dfc7b0e0bd4d323
Nick Jackson <nick@mysociety.org>
2013-08-27 14:10:58 +0100

    Add basics of Foundation framework.

--- templates/common_header.php
+++ templates/common_header.php
@@ -1,77 +1,81 @@
 <?php
 
-header("Content-Type: text/html; charset=utf-8"); 
-if (isset($values['cobrand'])) {
-  print cobrand_headers($values['cobrand'], 'common_header');
+header("Content-Type: text/html; charset=utf-8");
+
+if (isset($values['cobrand']))
+{
+    print cobrand_headers($values['cobrand'], 'common_header');
 }
- ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html lang="en">
+
+?><!DOCTYPE html>
+<!--[if IE 8]><html class="no-js lt-ie9" lang="en"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js" lang="en"><!--<![endif]-->
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>WriteToThem - <? print $values['title']; ?></title>
-<link href="/wtt.css" rel="stylesheet" type="text/css" media="all">
-<link href="/print.css" rel="stylesheet" type="text/css" media="print">
-<!--[if LT IE 7]>
-<style type="text/css">@import url("/ie6.css");</style>
-<![endif]-->
-<link rel="stylesheet" href="/static/js/fancybox/jquery.fancybox-1.3.4.css" type="text/css">
-<script src="/jslib/jquery-1.6.2.min.js"></script>
-<script src="/static/js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-<script>
-$(function(){
-    $('.fancybox').find('small').hide()
-      .end().each(function(){
-        $(this).fancybox({
-            href: $(this).prop('href').replace('#', '-')
-        });
-    });
-});
-</script>
 
-<?php if (array_key_exists('stylesheet', $values)) { ?>
-<style type="text/css">@import "<?=$values['stylesheet'] ?>";</style>
-<?php }
-      if (array_key_exists('robots', $values)) { ?>
-<meta name="robots" content="<?=$values['robots'] ?>">
-<?php }
-      if (OPTION_WEB_DOMAIN == 'writetothem.com') {
-?>
-<script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', '<?=OPTION_GOOGLE_ANALYTICS_TRACKING_CODE?>']);
-  _gaq.push(['_setDomainName', 'writetothem.com']);
-  _gaq.push(['_setAllowLinker', true]);
-  _gaq.push (['_gat._anonymizeIp']);
-  _gaq.push(['_trackPageview']);
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
-</script>
-<?php } ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <title>WriteToThem - <?=$values['title']?></title>
+
+    <link rel="stylesheet" href="/static/css/wtt.css">
+
+    <script src="/static/js/vendor/custom.modernizr.js"></script>
+
+    <?php if (array_key_exists('stylesheet', $values)): ?>
+    <style type="text/css">@import "<?=$values['stylesheet']?>";</style>
+    <?php
+
+    endif;
+
+    if (OPTION_WEB_DOMAIN == 'writetothem.com'):
+
+    ?>
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', '<?=OPTION_GOOGLE_ANALYTICS_TRACKING_CODE?>']);
+      _gaq.push(['_setDomainName', 'writetothem.com']);
+      _gaq.push(['_setAllowLinker', true]);
+      _gaq.push (['_gat._anonymizeIp']);
+      _gaq.push(['_trackPageview']);
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
+    <?php endif; ?>
 
 </head>
-<body<? if (isset($values['body_id'])) print ' id="' . $values['body_id'] . '"'; ?>><a name="top" id="top"></a>
+
+<body>
+
+<?php if (OPTION_FYR_REFLECT_EMAILS): ?>
+
+<div class="staging">
+    <div class="row">
+        <div class="large-12 columns">
+            <strong>This is a staging site.</strong> Emails will not be sent to representatives, but will instead be sent to you.
+        </div>
+    </div>
+</div>
+
+<?php endif; ?>
+
+<div class="banner-top">
+    <div class="row">
+        <div class="large-12 columns">
+            <a href="/"><img src="/static/img/logo.png" alt="WriteToThem"></a>
+        </div>
+    </div>
+</div>
+
+<a name="top" id="top"></a>
 <?php if (array_key_exists('header', $values)) {
           print $values['header'];
       } ?>
-<h1 id="heading">
-<a href="http://www.mysociety.org/"><img id="logo" alt="Visit mySociety.org" src="/images/mysociety-dark-50.png"><span id="logoie"></span></a>
 
-<? if ($_SERVER['REQUEST_URI']!='/') print '<a href="/">'; ?>
-WriteToThem<?
-if ($_SERVER['REQUEST_URI']!='/') print '</a>';
-if ($_SERVER['REQUEST_URI']!='/')
-    print ' <span id="homelink">(<a href="/">home</a>)</span>';
-if (OPTION_FYR_REFLECT_EMAILS) {
-    print ' <span id="betatest">Staging&nbsp;Site</span>';
-}
-?>
-</h1>
 <div id="content">
 <?
 	if (substr($real_template_name, 0, 5)=='about' && !isset($values['nobox'])) {
--- templates/website/footer.php
+++ templates/website/footer.php
@@ -1,31 +1,47 @@
 </div>
-<div id="footer">
-The new FaxYourMP.  
-<a href="/about-us">Built by mySociety</a>.  
-<a href="/about-copyright">Data by GovEval</a>.
-<a href="http://www.bytemark.co.uk/">Hosted by Bytemark</a>.
-<br>
 
-<? $links = array(
-    '/about-qa'=>'Help',
-    '/about-contact' => 'Contact WriteToThem.com',
-    /* '/about-guidelines' => 'Guidelines for Campaigners', */  /* we don't get much campaign abuse any more, so prominent link not so needed? */
-    '/lords' => 'Lords',
-    '/stats' => 'Statistics',
-    '/about-linktous' => 'Link to us',
-    '/about-copyright' => 'Copyright', /* for GNU Affero GPL */
-    '/about-privacy' => 'Privacy and Cookies',
-);
-foreach ($links as $uri => $text) {
-    $f = '';
-    if ($_SERVER['REQUEST_URI'] != $uri)
-        $f .= '<a href="' . $uri . '">';
-    $f .= $text;
-    if ($_SERVER['REQUEST_URI'] != $uri)
-        $f .= '</a>';
-    $footer[] = $f;
-}
-print join(' | ', $footer);
-echo '</div>';
+<div class="footer">
 
-echo '</body></html>';
+    <div class="row">
+
+        <div class="large-6 columns">
+
+            <ul class="inline-list">
+                <li><a href="/about-qa">Help</a></li>
+                <li><a href="/about-contact">Contact Us</a></li>
+                <li><a href="/about-copyright">Copyright</a></li>
+                <li><a href="/about-privacy">Privacy &amp; Cookies</a></li>
+                <li><a href="/about-linktous">Link To Us</a></li>
+                <li><a href="https://www.facebook.com/pages/Write-To-Them/204168472950434">Facebook</a></li>
+            </ul>
+
+        </div>
+
+        <div class="large-6 columns">
+
+            <ul class="inline-list">
+                <li><a href="/about-us">Built by mySociety</a></li>
+                <li><a href="/about-copyright">Data by GovEval</a></li>
+                <li><a href="http://www.bytemark.co.uk/">Hosted by Bytemark</a></li>
+            </ul>
+
+        </div>
+
+    </div>
+
+</div>
+
+<script>
+    document.write('<script src=' +
+    ('__proto__' in {} ? 'static/js/vendor/zepto' : 'static/js/vendor/jquery') +
+    '.js><\/script>')
+</script>
+
+<script src="static/js/foundation/foundation.js"></script>
+
+<script>
+    $(document).foundation();
+</script>
+
+</body>
+</html>
