b9b77675efdc4ac7a2ad4553a4cf4acc6c2bd040
Nick Jackson <nick@nickjackson.me>
2013-12-10 09:30:59 +0000

    Update Google Analytics to use Universal tracking code.

--- templates/common_header.php
+++ templates/common_header.php
@@ -44,20 +44,20 @@ if (isset($values['cobrand']))
     <?php endif; ?>
 
     <?php if (OPTION_WEB_DOMAIN == 'writetothem.com'): ?>
-    <script type="text/javascript">
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', '<?=OPTION_GOOGLE_ANALYTICS_TRACKING_CODE?>']);
-      _gaq.push(['_setDomainName', 'writetothem.com']);
-      _gaq.push(['_setAllowLinker', true]);
-      _gaq.push (['_gat._anonymizeIp']);
-      _gaq.push(['_trackPageview']);
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
+
+    <script>
+
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', '<?=OPTION_GOOGLE_ANALYTICS_TRACKING_CODE?>', 'writetothem.com');
+        ga('set', 'anonymizeIp', true);
+        ga('send', 'pageview');
 
     </script>
+
     <?php endif; ?>
 
 </head>
