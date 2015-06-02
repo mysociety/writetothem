mysociety/writetothem

commit e05dc1c0ba424cef803500fc22e1f7d5a9b424e7

Move A/B testing to the page title.

templates/common_header.php
@@ -46,7 +46,7 @@ if (isset($values['cobrand']))
     <?php if (OPTION_WEB_DOMAIN == 'writetothem.com'): ?>
 
     <!-- Load GA experiments API -->
-    <script src="//www.google-analytics.com/cx/api.js?experiment=Daii1FTKSMG0Lo2lQoQUBw"></script>
+    <script src="//www.google-analytics.com/cx/api.js?experiment=9AJDuy1gQjiG51CmMpaVlg"></script>
 
     <script>
 
@@ -57,13 +57,13 @@ if (isset($values['cobrand']))
         var pageVariations = [
 
             function() {    // Original
-                document.getElementById('subtitle').innerHTML = 'Over 200,000 messages sent last year.';
+                document.getElementById('title').innerHTML = 'Write to your politicians, national or local, for free.';
             },
             function() {    // Variant 1
-                document.getElementById('subtitle').innerHTML = 'It takes less than five minutes.';
+                document.getElementById('title').innerHTML = 'Email your MP, local councillors or other representatives.';
             },
             function() {    // Variant 2
-                document.getElementById('subtitle').innerHTML = 'It takes less than 5 minutes.';
+                document.getElementById('title').innerHTML = 'Email the people in power.';
             }
         ];
 

templates/website/footer.php
@@ -52,7 +52,11 @@
 
     // When page is ready, switch the content
     $(document).ready(
-        pageVariations[chosenVariation]
+        try {
+            pageVariations[chosenVariation]
+        } catch(e) {
+            document.getElementById('title').innerHTML = 'Write to your politicians, national or local, for free.';
+        }
     );
 
     <?php endif; ?>

web/index.php
@@ -344,7 +344,7 @@ if ($cobrand && file_exists("../../data/cobrand.csv")) {
 template_draw($template, array(
         'body_id' => 'home',
         "title" => $title,
-        "blurb-top" => '<h2>Write to your politicians, national or local, for free.</h2><p id="subtitle">&nbsp;</p>',
+        "blurb-top" => '<h2 id="title">&nbsp;<noscript>Write to your politicians, national or local, for free.</noscript></h2><p>Over 200,000 messages sent last year.</p>',
         "form" => $form,
         "error" => $error_message,
         "all_url" => $fyr_all_url,

commit 97e22b944749cc7d47815df04815db82a423952a

Fix JavaScript errors on non-front page.
e05dc1c0 introduced JavaScript error on all non-front pages, by trying
to set the innerHTML of a non existent element. 5dcd3f25 did not fix it,
as the catch had the same error.

templates/common_header.php
@@ -45,28 +45,30 @@ if (isset($values['cobrand']))
 
     <?php if (OPTION_WEB_DOMAIN == 'writetothem.com'): ?>
 
-    <!-- Load GA experiments API -->
-    <script src="//www.google-analytics.com/cx/api.js?experiment=9AJDuy1gQjiG51CmMpaVlg"></script>
+    <?php if ($_SERVER['REQUEST_URI'] == '/') { ?>
+        <!-- Load GA experiments API -->
+        <script src="//www.google-analytics.com/cx/api.js?experiment=9AJDuy1gQjiG51CmMpaVlg"></script>
+        <script>
+            // Select GA experiment variation
+            var chosenVariation = cxApi.chooseVariation();
+
+            // Text variations to use
+            var pageVariations = [
+
+                function() {    // Original
+                    document.getElementById('title').innerHTML = 'Write to your politicians, national or local, for free.';
+                },
+                function() {    // Variant 1
+                    document.getElementById('title').innerHTML = 'Email your MP, local councillors or other representatives.';
+                },
+                function() {    // Variant 2
+                    document.getElementById('title').innerHTML = 'Email the people in power.';
+                }
+            ];
+        </script>
+    <?php } ?>
 
     <script>
-
-        // Select GA experiment variation
-        var chosenVariation = cxApi.chooseVariation();
-
-        // Text variations to use
-        var pageVariations = [
-
-            function() {    // Original
-                document.getElementById('title').innerHTML = 'Write to your politicians, national or local, for free.';
-            },
-            function() {    // Variant 1
-                document.getElementById('title').innerHTML = 'Email your MP, local councillors or other representatives.';
-            },
-            function() {    // Variant 2
-                document.getElementById('title').innerHTML = 'Email the people in power.';
-            }
-        ];
-
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)

templates/website/footer.php
@@ -49,12 +49,14 @@
     $(document).foundation();
 
     // When page is ready, switch the content
-    try {
-        $(document).ready(
-            pageVariations[chosenVariation]
-        );
-    } catch(err) {
-        document.getElementById('title').innerHTML = 'Write to your politicians, national or local, for free.';
+    if (typeof pageVariations !== 'undefined') {
+        try {
+            $(document).ready(
+                pageVariations[chosenVariation]
+            );
+        } catch(err) {
+            document.getElementById('title').innerHTML = 'Write to your politicians, national or local, for free.';
+        }
     }
 
 </script>