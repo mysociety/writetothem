mysociety/writetothem

commit 7844e6a1f696ec5a55c7a72558889d32aafabd1d

Modify design to use new restricted width, introduce A/B variants for home and rep selection pages.

templates/common_header.php
@@ -14,7 +14,7 @@ if (isset($values['cobrand']))
 
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
     <?php if (array_key_exists('robots', $values)): ?>
     <meta name="robots" content="<?=$values['robots']?>">
@@ -23,14 +23,14 @@ if (isset($values['cobrand']))
     <title>WriteToThem - <?=$values['title']?></title>
 
     <link rel="stylesheet" href="/static/css/wtt.css">
-    
+
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <link rel="icon" type="image/png" href="/static/img/favicon-128.png">
     <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/static/img/favicon-57.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/static/img/favicon-72.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/static/img/favicon-114.png">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/static/img/favicon-144.png">
-    
+
     <meta property="og:title" content="WriteToThem">
     <meta property="og:type" content="website">
     <meta property="og:url" content="<?=OPTION_BASE_URL?>">
@@ -72,25 +72,37 @@ if (isset($values['cobrand']))
 
 <div class="staging">
     <div class="row">
-        <div class="large-12 columns">
+        <div class="large-10 large-centered columns">
             <strong>This is a staging site.</strong> Emails will not be sent to representatives, but will instead be sent to you.
         </div>
     </div>
 </div>
 
+<?php endif; ?>
+
+<div class="content-wrapper">
+<div class="row">
+    <div class="large-12 columns content">
+
 <?php
 
-endif;
-
-if ( ( ! isset($values['template']) OR $values['template'] !== 'index-index') AND ! isset($values['skip_header']) ):
+if ( ( ! isset($values['template']) OR ( $values['template'] !== 'index-index' AND $values['template'] !== 'index-index-b' ) ) AND ! isset($values['skip_header']) ):
 
 ?>
 
-<div class="banner-top">
-    <div class="row">
-        <div class="large-12 columns">
-            <a href="/"><img src="/static/img/logo.png" data-interchange="[/static/img/logo@2x.png, (retina)]" alt="WriteToThem"></a>
+
+
+
+<div class="row banner-top">
+    <div class="large-10 large-centered columns">
+        <div class="ms_header_nav">
+            <nav>
+                <ul class="menu">
+                    <li id="ms_logo"><a class="ms_header_nav-logo" target="_blank" href="http://www.mysociety.org">&nbsp;</a></li>
+                </ul>
+            </nav>
         </div>
+        <a href="/"><img src="/static/img/logo.png" data-interchange="[/static/img/logo@2x.png, (retina)]" alt="WriteToThem"></a>
     </div>
 </div>
 

web/index.php
@@ -37,7 +37,7 @@ function postcode_form($pc, $cobrand, $cocode, $a_forward, $error_message, $opti
     $message = cobrand_enter_postcode_message($cobrand, $cocode);
 
     if (!$message) {
-        $message = 'First, type your UK postcode:';
+        $message = 'First, enter your UK postcode:';
     }
 
     $form .= $message;
@@ -344,7 +344,7 @@ if ($cobrand && file_exists("../../data/cobrand.csv")) {
 template_draw($template, array(
         'body_id' => 'home',
         "title" => $title,
-        "blurb-top" => '<h2>The simple way to contact your representative</h2>' . $blurb_top,
+        "blurb-top" => '<h2>Write to your politicians, national or local, for free.</h2><p>Over 200,000 messages sent last year.</p>',
         "form" => $form,
         "error" => $error_message,
         "all_url" => $fyr_all_url,

templates/website/about-branded.html
@@ -5,17 +5,18 @@ template_draw('header', $values);
 
 ?>
 
-<div class="content">
+<div class="row">
+    <div class="large-10 large-centered columns">
 
-    <div class="row">
-        <div class="large-12 columns">
-            <h2>WriteToThem for your website</h2>
+        <div class="row">
+            <div class="large-12 columns">
+                <h2>WriteToThem for your website</h2>
+            </div>
         </div>
-    </div>
 
-    <div class="row">
+        <div class="row">
 
-        <div class="large-8 columns">
+            <div class="large-8 columns">
 
 <p>If you just want to link to WriteToThem to tell other people about it, that&rsquo;s <a href="http://www.writetothem.com/about-linktous">easy</a>. But maybe you want something shinier than that &ndash; you&rsquo;ve designed a brand new website for your campaign, you want people to write to their MPs to encourage them to support it, and you&rsquo;d absolutely love this to happen without people needing to leave your website at all.</p>
 
@@ -27,14 +28,15 @@ template_draw('header', $values);
 
 <p>If you have any problems installing the code, <a href="/about-contact">contact us</a> and we&rsquo;ll give you a hand.</p>
 
+            </div>
+
+            <div class="large-4 columns">
+
+                <?php template_draw('about-sidebar'); ?>
+
+            </div>
+
         </div>
-
-        <div class="large-4 columns">
-
-            <?php template_draw('about-sidebar'); ?>
-
-        </div>
-
     </div>
 </div>
 

templates/website/about-campaigns.html
@@ -5,17 +5,18 @@ template_draw('header', $values);
 
 ?>
 
-<div class="content">
+<div class="row">
+    <div class="large-10 large-centered columns">
 
-    <div class="row">
-        <div class="large-12 columns">
-            <h2>Running a campaign</h2>
+        <div class="row">
+            <div class="large-12 columns">
+                <h2>Running a campaign</h2>
+            </div>
         </div>
-    </div>
 
-    <div class="row">
+        <div class="row">
 
-        <div class="large-8 columns">
+            <div class="large-8 columns">
 
 <p>We quite often get emails from campaign groups asking how they can use our site to mobilise support. Here&rsquo;s how.</p>
 
@@ -34,14 +35,15 @@ template_draw('header', $values);
 
 <p>For a more seamless experience you might want to <a href="/about-branded">create your own branded webpage for the campaign</a>, and there&rsquo;s code available to help you do that. Don&rsquo;t be daunted &ndash; you don&rsquo;t need to be a programmer to install it, and there are easy step-by-step instructions.</p>
 
+            </div>
+
+            <div class="large-4 columns">
+
+                <?php template_draw('about-sidebar'); ?>
+
+            </div>
+
         </div>
-
-        <div class="large-4 columns">
-
-            <?php template_draw('about-sidebar'); ?>
-
-        </div>
-
     </div>
 </div>
 

templates/website/about-constituency.html
@@ -5,17 +5,18 @@ template_draw('header', $values);
 
 ?>
 
-<div class="content">
+<div class="row">
+    <div class="large-10 large-centered columns">
 
-    <div class="row">
-        <div class="large-12 columns">
-            <h2>What postcode should I use?</h2>
+        <div class="row">
+            <div class="large-12 columns">
+                <h2>What postcode should I use?</h2>
+            </div>
         </div>
-    </div>
 
-    <div class="row">
+        <div class="row">
 
-        <div class="large-8 columns">
+            <div class="large-8 columns">
 
 <p>If you're not sure what postcode to use, or you don't know your
 postcode, this page will help.
@@ -129,14 +130,15 @@ that in Northern Ireland electoral registration is the responsibility of the
 <a href="http://www.electoralofficeni.gov.uk/">Electoral Office for Northern
 Ireland</a> who can give more specific advice.)</p>
 
+            </div>
+
+            <div class="large-4 columns">
+
+                <?php template_draw('about-sidebar'); ?>
+
+            </div>
+
         </div>
-
-        <div class="large-4 columns">
-
-            <?php template_draw('about-sidebar'); ?>
-
-        </div>
-
     </div>
 </div>
 

templates/website/about-contact.html
@@ -6,17 +6,18 @@ template_draw('header', $values);
 
 ?>
 
-<div class="content">
+<div class="row">
+    <div class="large-10 large-centered columns">
 
-    <div class="row">
-        <div class="large-12 columns">
-            <h2>Emailing WriteToThem</h2>
+        <div class="row">
+            <div class="large-12 columns">
+                <h2>Emailing WriteToThem</h2>
+            </div>
         </div>
-    </div>
 
-    <div class="row">
+        <div class="row">
 
-        <div class="large-8 columns">
+            <div class="large-8 columns">
 
 <p>WriteToThem is an independent, <a href="about-us">charitable project</a>
 to help the public contact their elected representatives.</p>
@@ -62,14 +63,15 @@ href="mailto:<?=$contact_email?>"><?=$contact_email?></a> for prompt
 action.
 </p>
 
+            </div>
+
+            <div class="large-4 columns">
+
+                <?php template_draw('about-sidebar'); ?>
+
+            </div>
+
         </div>
-
-        <div class="large-4 columns">
-
-            <?php template_draw('about-sidebar'); ?>
-
-        </div>
-
     </div>
 </div>
 

templates/website/about-contactresponse.html
@@ -5,28 +5,30 @@ template_draw('header', $values);
 
 ?>
 
-<div class="content">
+<div class="row">
+    <div class="large-10 large-centered columns">
 
-    <div class="row">
-        <div class="large-12 columns">
-            <h2>Emailing WriteToThem.com</h2>
-        </div>
-    </div>
-
-    <div class="row">
-
-        <div class="large-8 columns">
-
-<? fyr_display_emailform($values); ?>
-
+        <div class="row">
+            <div class="large-12 columns">
+                <h2>Emailing WriteToThem.com</h2>
+            </div>
         </div>
 
-        <div class="large-4 columns">
+        <div class="row">
 
-            <?php template_draw('about-sidebar'); ?>
+            <div class="large-8 columns">
+
+    <? fyr_display_emailform($values); ?>
+
+            </div>
+
+            <div class="large-4 columns">
+
+                <?php template_draw('about-sidebar'); ?>
+
+            </div>
 
         </div>
-
     </div>
 </div>
 

templates/website/about-copyright.html
@@ -5,17 +5,18 @@ template_draw('header', $values);
 
 ?>
 
-<div class="content">
+<div class="row">
+    <div class="large-10 large-centered columns">
 
-    <div class="row">
-        <div class="large-12 columns">
-            <h2>Copyright information</h2>
+        <div class="row">
+            <div class="large-12 columns">
+                <h2>Copyright information</h2>
+            </div>
         </div>
-    </div>
 
-    <div class="row">
+        <div class="row">
 
-        <div class="large-8 columns">
+            <div class="large-8 columns">
 
 <h3><a href="http://www.mysociety.org"><img src="images/mysociety_sm.gif"
 alt="mySociety" border="0"></a></h3>
@@ -38,14 +39,15 @@ using <a href="http://mapit.mysociety.org/">MaPit</a>, our tool for
 doing that sort of thing, and uses Ordnance Survey open data behind the scenes.
 Thanks to them both!
 
+            </div>
+
+            <div class="large-4 columns">
+
+                <?php template_draw('about-sidebar'); ?>
+
+            </div>
+
         </div>
-
-        <div class="large-4 columns">
-
-            <?php template_draw('about-sidebar'); ?>
-
-        </div>
-
     </div>
 </div>
 

commit d881e60edfbd39ca2ac1d5645fa75f769db65663

Fix header for limited-representative front page.
7844e6a1 broke the heading on home pages that utilised the ‘a’ variable,
not listing only the representatives being looked up.

templates/common_header.php
@@ -45,7 +45,10 @@ if (isset($values['cobrand']))
 
     <?php if (OPTION_WEB_DOMAIN == 'writetothem.com'): ?>
 
-    <?php if ($_SERVER['REQUEST_URI'] == '/') { ?>
+    <?php
+        $url = parse_url($_SERVER['REQUEST_URI']);
+        if ($url['path'] == '/' && isset($values['experiment']) && $values['experiment']) {
+    ?>
         <!-- Load GA experiments API -->
         <script src="//www.google-analytics.com/cx/api.js?experiment=9AJDuy1gQjiG51CmMpaVlg"></script>
         <script>

web/index.php
@@ -292,9 +292,10 @@ if ($cobrand){
     $title = cobrand_step_title($cobrand, 1);
 }
 if ($title == ''){
-    $title = "Email your Councillor, MP, MEP, MSP or Welsh, NI, London Assembly Member for free";
+    $title = "Email your Councillor, MP, MEP, MSP or Welsh, NI, or London Assembly Member for free";
 }
-$blurb_top = '<p>Councillors, <abbr title="Member of Parliament">MP</abbr>, <abbr title="Members of the European Parliament">MEPs</abbr>, <abbr title="Members of the Scottish Parliament">MSPs</abbr>, or <br>Northern Ireland, Welsh and London <abbr title="Assembly Members">AMs</abbr>.</p>';
+
+$blurb_top = '<h2 id="title">&nbsp;<noscript>Write to your politicians, national or local, for free.</noscript></h2><p>Over 200,000 messages sent last year.</p>';
 
 $fyr_all_url = null;
 $area_types = null;
@@ -309,18 +310,10 @@ if ($a_forward) {
                         'fyr_extref', fyr_external_referrer(),
                         'cocode', get_http_var('cocode')));
         }
+        $blurb_top = "<h2>Write to your $area_type_desc</h2><p>Over 200,000 messages sent last year.</p>";
     }
 }
-if (!$area_types) {
-    # All representatives
-    global $va_child_types;
-    $area_types = array();
-    foreach ($va_child_types as $child)
-        if ($child != 'HOC')
-            $area_types[$child] = 1;
-    $area_type_desc = fyr_describe_area_type_list($area_types);
-}
-$blurb_top = "<p>$area_type_desc</p>";
+
 if ($template != 'index-advice') {
     header('Cache-Control: max-age=3600');
 }
@@ -343,9 +336,10 @@ if ($cobrand && file_exists("../../data/cobrand.csv")) {
 
 // Display page
 template_draw($template, array(
+        'experiment' => !$area_types,
         'body_id' => 'home',
         "title" => $title,
-        "blurb-top" => '<h2 id="title">&nbsp;<noscript>Write to your politicians, national or local, for free.</noscript></h2><p>Over 200,000 messages sent last year.</p>',
+        "blurb-top" => $blurb_top,
         "form" => $form,
         "error" => $error_message,
         "all_url" => $fyr_all_url,