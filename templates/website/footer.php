821b9ce497b89e9b3e2b202b6bc9e884ba100448
Zarino Zappia <mail@zarino.co.uk>
2016-04-28 16:24:25 +0100

    Replace footer with standard mySociety footer
    Other than the obvious adding of _ms_footer.scss and its
    settings at the bottom of wtt.scss, the rest of the changes
    centre around tweaking the margin between page content and
    the footer, which required some background colour changes
    to stop the body/content background colour leaking through
    the footer’s margin-top.
    
    Fixes #307.

--- templates/website/footer.php
+++ templates/website/footer.php
@@ -1,33 +1,67 @@
-<div class="row footer">
+<div class="mysoc-footer row" role="contentinfo">
     <div class="large-10 large-centered columns" role="navigation">
         <div class="row">
-            <div class="footer-content">
 
-                <div class="large-6 columns">
-
-                    <ul class="inline-list footer-links-main">
-                        <li><a href="/about-qa">Help</a></li>
-                        <li><a href="/about-contact">Contact Us</a></li>
-                        <li><a href="/about-copyright">Copyright</a></li>
-                        <li><a href="/about-privacy">Privacy &amp; Cookies</a></li>
-                        <li><a href="/about-linktous">Link To Us</a></li>
-                        <li><a href="https://www.facebook.com/mysociety">Facebook</a></li>
-                    </ul>
-
+            <div class="large-5 columns">
+                <h2 class="mysoc-footer__site-name">WriteToThem</h2>
+                <div class="mysoc-footer__site-description">
+                    <p>Making it easy to write to the politicians who represent you &ndash; even if you don&rsquo;t know who they are.</p>
                 </div>
-
-                <div class="large-6 columns">
-
-                    <ul class="inline-list footer-links-credits">
-                        <li><a href="/about-us">Built by mySociety</a></li>
-                        <li><a href="/about-copyright">Data by GovEval</a></li>
-                        <li><a href="http://www.bytemark.co.uk/">Hosted by Bytemark</a></li>
-                        <?php if(isset($values['credit'])) echo '<li>' . $values['credit'] . '</li>'; ?>
-                    </ul>
-
-                </div>
-
             </div>
+
+            <div class="large-4 columns">
+                <nav class="mysoc-footer__links">
+                    <ul>
+                        <li role="presentation"><a href="/about-qa">Help</a></li>
+                        <li role="presentation"><a href="/about-contact">Contact Us</a></li>
+                        <li role="presentation"><a href="/about-linktous">Link To Us</a></li>
+                    </ul>
+                    <ul>
+                        <li role="presentation"><a href="/about-copyright">Copyright</a></li>
+                        <li role="presentation"><a href="/about-privacy">Privacy &amp; Cookies</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+            <div class="large-3 columns">
+                <div class="mysoc-footer__donate">
+                    <p>Your donations keep this site and others like it running</p>
+                    <a href="https://www.mysociety.org/donate" class="mysoc-footer__donate__button">Donate now</a>
+                </div>
+            </div>
+
+        </div>
+        <hr class="mysoc-footer__divider" role="presentation">
+        <div class="row">
+
+            <div class="large-5 columns">
+                <div class="mysoc-footer__orgs">
+                    <p class="mysoc-footer__org">
+                        Built by
+                        <a href="https://www.mysociety.org" class="mysoc-footer__org__logo mysoc-footer__org__logo--mysociety">mySociety</a>
+                    </p>
+                </div>
+            </div>
+
+            <div class="large-4 columns">
+                <div class="mysoc-footer__legal">
+                    <p>
+                        <a href="/about-copyright">Data by GovEval</a>.
+                        <a href="http://www.bytemark.co.uk/">Hosted by Bytemark</a>.
+                        <?php if(isset($values['credit'])) echo $values['credit']; ?>.
+                    </p>
+                    <p>mySociety Limited is a project of UK Citizens Online Democracy, a registered charity in England and Wales. For full details visit <a href="https://www.mysociety.org">mysociety.org</a>.</p>
+                </div>
+            </div>
+
+            <div class="large-3 columns">
+                <ul class="mysoc-footer__badges">
+                    <li role="presentation"><a href="https://github.com/mysociety/writetothem/" class="mysoc-footer__badge mysoc-footer__badge--github">Github</a></li>
+                    <li role="presentation"><a href="https://twitter.com/mysociety" class="mysoc-footer__badge mysoc-footer__badge--twitter">Twitter</a></li>
+                    <li role="presentation"><a href="https://www.facebook.com/mysociety" class="mysoc-footer__badge mysoc-footer__badge--facebook">Facebook</a></li>
+                </ul>
+            </div>
+
         </div>
     </div>
 </div>
