67d3b6c8332b0ba33cdcf5bc2afcfd042c329b8b
Zarino Zappia <mail@zarino.co.uk>
2018-10-11 17:26:26 +0100

    Improved contact page
    Resurrecting some code from a 2018 (!!) prototype, to reduce misdirected
    contact form / user support messages.
    
    Part of #369.

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -15,4 +15,34 @@ $(function() {
     });
 
     $('.js-fixed-thead').fixedThead();
+
+    var toggleContactOption = function( $option, show ) {
+        var $siblings = $option.siblings('.contact-option');
+        var $btn = $option.find('h3 button');
+        var $content = $option.find('.contact-option__detail');
+
+        if ( show ) {
+            $btn.attr('aria-expanded', 'true');
+            $content.removeAttr('hidden');
+            $option.addClass('contact-option--active');
+            $siblings.each(function(){
+                toggleContactOption( $(this), false );
+            });
+        } else {
+            $btn.attr('aria-expanded', 'false');
+            $content.attr('hidden', 'hidden');
+            $option.removeClass('contact-option--active');
+        }
+    };
+
+    $('.js-contact-options-accordion .contact-option').each(function(){
+        var $option = $(this);
+        var $btn = $(this).find('h3 button');
+
+        toggleContactOption( $option, false );
+
+        $btn.on('click', function(){
+            toggleContactOption( $option, $btn.attr('aria-expanded') === 'false' );
+        });
+    });
 });
