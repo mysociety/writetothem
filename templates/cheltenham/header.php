b569cc741dcd10d5f8d94cc71cd1c775c65039ba
M Somerville <matthew-github@dracos.co.uk>
2021-03-17 14:35:22 +0000

    Remove unused code.

--- templates/cheltenham/header.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$values['stylesheet'] = 'cobrands/cheltenham/cheltenham.css';
-$values['header'] = '<a title="Back to Cheltenham Council website" href="http://www.cheltenham.gov.uk/"><img id="cobrand_logo" alt="Return to www.cheltenham.gov.uk" src="http://www.cheltenham.gov.uk/libraries/images/logo.gif"></a>';
-include_once '../templates/common_header.php';
-
-?>
--- templates/foiorder2009/header.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-include_once '../templates/common_header.php';
-
-?>
