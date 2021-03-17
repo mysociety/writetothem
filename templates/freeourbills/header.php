60aa5eb562f3aa36ed3454b96508d590f0dcff36
M Somerville <matthew-github@dracos.co.uk>
2021-03-17 14:35:22 +0000

    Remove unused code.

--- templates/freeourbills/header.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$values['stylesheet'] = '/cobrands/freeourbills/freeourbills.css';
-$values['header'] = '<a title="To Free Our Bills website" href="http://www.theyworkforyou.com/freeourbills"><img id="cobrand_logo" alt="Return to Free Our Bills" src="http://www.theyworkforyou.com/freeourbills/billsm.png"></a>';
-include_once '../templates/common_header.php';
-
-?>
