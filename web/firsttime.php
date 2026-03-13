dbf2cb8be6a4f59e8c89941feb0a3eb891af134d
Alex Parsons <alex.parsons@mysociety.org>
2026-03-13 16:36:22 +0000

    Expand firsttime question to full survey form

--- web/firsttime.php
+++ web/firsttime.php
@@ -22,42 +22,63 @@ $token = get_http_var('token');
 if (!$token) {
     template_show_error("The token was missing.");
 }
-$answer = get_http_var('answer');
-if ($answer != "yes" && $answer != "no") {
-    template_show_error("The answer type was missing.");
-}
 
-// 1 is the firsttime question
-$result = msg_record_questionnaire_answer($token, 1, $answer);
-if (rabx_is_error($result)) {
-    template_show_error($result->text);
-}
-$values = msg_admin_get_message($result);
-$values['cobrand'] = $cobrand;
+$preview = ($token === 'preview');
 
+// Handle service questions form submission (Q1 + Q2-Q4)
+$submitted = get_http_var('submitted');
+if ($submitted) {
+    if (!$preview) {
+        // Record Q1: first-time question
+        $answer = get_http_var('answer');
+        if ($answer == "yes" || $answer == "no") {
+            $result = msg_record_questionnaire_answer($token, 1, $answer);
+            if (rabx_is_error($result)) {
+                template_show_error($result->text);
+            }
+        }
 
-// SurveyGizmo survey hook
-// Current url for local councillor name recognition survey
-// Survey disabled
-// https://www.surveygizmo.com/s3/5057760/WTT-Name-Recognition-Local-Council
+        // Record Q2-Q4: Likert service questions
+        $valid_likert = array('strongly_agree', 'agree', 'neutral', 'disagree', 'strongly_disagree');
+        foreach (array(2 => 'q2', 3 => 'q3', 4 => 'q4') as $qn => $param) {
+            $val = get_http_var($param);
+            if ($val && in_array($val, $valid_likert)) {
+                $result = msg_record_questionnaire_answer($token, $qn, $val);
+                if (rabx_is_error($result)) {
+                    template_show_error($result->text);
+                }
+            }
+        }
 
-$local_council_codes = ['DIW','CED','LBW','COP','LGE','MTW','UTE','UTW'];
-$wrote_to_councillor = in_array($values["recipient_type"], $local_council_codes);
+        // Record Q5: NPS (integer 0-10)
+        $nps = get_http_var('q5');
+        if ($nps !== '' && $nps !== false && ctype_digit(strval($nps)) && (int)$nps >= 0 && (int)$nps <= 10) {
+            $result = msg_record_questionnaire_answer($token, 5, strval((int)$nps));
+            if (rabx_is_error($result)) {
+                template_show_error($result->text);
+            }
+        }
+    }
 
-//send no surveys
-$send_to_survey_gizmo = 0; 
-// use rand(0, 1); for a 50% referral rate 
-
-if ($send_to_survey_gizmo) {
-    $values['title'] = "Help us by answering a few quick questions";
-    $values['survey_url'] = 'https://www.surveygizmo.com/s3/' .
-	'5057760/WTT-Name-Recognition-Local-Council' . 
-	'?__no_style=true&amp;' .
-	'__ref=WTT-Name-Recognition-Local-Councily&amp;' .
-	'recipient_id=' . $values['recipient_id'] . '&amp;' .
-	'first_use=' . ($values['questionnaire_1_yes'] ? 'true' : 'false');
-    template_draw("surveygizmo-survey", $values);
-} else {
-    // Questionnaire done
+    // All done
+    $values = array('cobrand' => $cobrand, 'cocode' => $cocode, 'host' => fyr_get_host());
     template_draw("survey-done", $values);
+    exit;
 }
+
+// Show combined form with first-time question + service questions
+// Randomly select 2 from the pool of optional service questions
+$service_question_pool = array(2, 3, 4, 5);
+shuffle($service_question_pool);
+$service_questions = array_slice($service_question_pool, 0, 2);
+sort($service_questions);
+
+$values = array(
+    'token' => $token,
+    'response' => get_http_var('response'),
+    'service_questions' => $service_questions,
+    'cobrand' => $cobrand,
+    'cocode' => $cocode,
+    'host' => fyr_get_host(),
+);
+template_draw("service-questions", $values);
