b98432e14a62cc54e54095262c88fc19296bf348
Lucas <hello@lucascm.dev>
2026-06-22 09:04:52 +0100

    Added Donation button to header
    - Added also a red button class.

--- templates/common_header.php
+++ templates/common_header.php
@@ -135,7 +135,10 @@ if (isset($values['cobrand']))
             <div class="row banner-top">
                 <div class="large-10 large-centered columns">
                     <a href="/" class="wtt-logo"><img src="/static/img/logo.png" style="max-width:250px;max-height:100%;" alt="WriteToThem"></a>
-                    <a href="/about-qa" target="_blank" class="wtt-help" title="<?= _('Opens in a new window') ?>"><?= _('Help') ?></a>
+                    <div class="header-content">
+                        <a href="/about-qa" target="_blank" class="header-link" title="<?= _('Opens in a new window') ?>"><?= _('Help') ?></a>
+                        <a href="https://www.mysociety.org/donate/support-writetothem-and-mysociety/?utm_source=writetothem.com&amp;utm_content=header+donate+now&amp;utm_medium=link&amp;utm_campaign=mysoc_header" class="button red small radius">Donate now</a>
+                    </div>
                 </div>
             </div>
           <?php endif; ?>
