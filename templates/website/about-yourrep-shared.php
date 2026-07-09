7278716fc6b88463ffba99779efbd5a79aaea165
Alex Parsons <alex.parsons@mysociety.org>
2026-07-09 14:48:54 +0000

    Switch about your rep markdown to use new common function
    - Main difference is parsedown doesn't need to be passed in anymore

--- templates/website/about-yourrep-shared.php
+++ templates/website/about-yourrep-shared.php
@@ -1,22 +1,13 @@
 <?php
 
-$pd = new Parsedown();
+require_once __DIR__ . '/about-markdown.php';
 
 /**
- * Load a markdown file with language awareness.
- * Tries LANGUAGE first (e.g. cy), falls back to en.
+ * Load a yourrep markdown file from templates/website/yourrep-md.
+ * Language-aware with an English fallback (see render_md).
  */
-function yourrep_md($filename, $pd) {
-    $base = __DIR__ . '/yourrep-md';
-    $lang = defined('LANGUAGE') ? LANGUAGE : 'en';
-    $path = "$base/$lang/$filename";
-    if ($lang !== 'en' && !file_exists($path)) {
-        $path = "$base/en/$filename";
-    }
-    if (!file_exists($path)) {
-        return '';
-    }
-    return $pd->text(file_get_contents($path));
+function yourrep_md($filename) {
+    return render_md($filename, 'yourrep-md');
 }
 
 ?>
