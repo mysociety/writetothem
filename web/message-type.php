d0c9525b7c378b9351f19d22756a8fed1bd959b0
Alex Parsons <alex.parsons@mysociety.org>
2026-07-12 20:46:45 +0000

    Pass all values onto the write page
    If values have been pre-populated, store to pass over to write page

--- web/message-type.php
+++ web/message-type.php
@@ -13,32 +13,41 @@ require_once "../phplib/fyr.php";
 require_once "../commonlib/phplib/utility.php";
 
 $who = get_http_var('who');
-$pc = get_http_var('pc');
-$type = get_http_var('type');
-$fyr_extref = get_http_var('fyr_extref');
-$cocode = get_http_var('cocode');
-
 if (!$who) {
     header('Location: /');
     exit;
 }
 
-// Build the URL to the write page, preserving all parameters
-$write_params = array('who' => $who, 'pc' => $pc);
-if ($type) $write_params['type'] = $type;
-if ($fyr_extref) $write_params['fyr_extref'] = $fyr_extref;
-if ($cocode) $write_params['cocode'] = $cocode;
+$write_url = cobrand_url($cobrand, '/write', get_http_var('cocode'));
 
-$write_url = cobrand_url($cobrand, '/write', $cocode);
+/* whitelist of parameters to forward to the write page */
+$forwarded_param_names = array(
+    'who', 'type', 'pc', 'cocode', 'fyr_extref',
+    'writer_name', 'writer_address1', 'writer_address2',
+    'writer_town', 'writer_county', 'writer_email', 'writer_phone',
+);
+
+$write_params = array();
+foreach ($forwarded_param_names as $name) {
+    $value = get_http_var($name);
+    if ($value !== '') {
+        $write_params[$name] = $value;
+    }
+}
+
+/* If the incoming link already says what the message is about, skip this
+ * screen and go straight to writing, taking the whitelisted parameters
+ * (plus message_type) along. */
+$message_type = get_http_var('message_type');
+if (in_array($message_type, array('casework', 'campaigning', 'other'), true)) {
+    $redirect_params = array_merge($write_params, array('message_type' => $message_type));
+    header('Location: ' . $write_url . '?' . http_build_query($redirect_params));
+    exit;
+}
 
 template_draw('message-type', array(
     'write_url' => $write_url,
     'write_params' => $write_params,
-    'who' => $who,
-    'pc' => $pc,
-    'type' => $type,
-    'fyr_extref' => $fyr_extref,
-    'cocode' => $cocode,
     'cobrand' => $cobrand,
     'host' => fyr_get_host(),
 ));
